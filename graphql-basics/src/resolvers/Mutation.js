79ed39a9a0e8e161dfbae80ff97cca9482b53f55
projectEscape <escaperealityproject@gmail.com>
2019-07-03 02:58:11 +0530

    update mutations

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -39,6 +39,24 @@ const Mutation = {
 
     return deletedUsers[0];
   },
+  updateUser(parent, { id, data }, { db }, info) {
+    const user = db.users.find(user => user.id === id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    if (typeof data.email === "string") {
+      const emailTaken = db.users.some(user => user.email === data.email);
+      if (emailTaken) throw new Error("Email Taken");
+      user.email = data.email;
+    }
+    if (typeof data.name === "string") {
+      user.name = data.name;
+    }
+    if (typeof data.age !== "undefined") {
+      user.age = data.age;
+    }
+    return user;
+  },
   createPost(parent, { data }, { db }, info) {
     const userExists = db.users.some(user => {
       return user.id === data.author;
@@ -65,6 +83,19 @@ const Mutation = {
     db.comments = db.comments.filter(comment => comment.post !== args.id);
 
     return deletedPost[0];
+  },
+  updatePost(parent, { id, data }, { db }, info) {
+    const post = db.posts.find(post => post.id === id);
+    if (typeof data.title === "string") {
+      post.title = data.title;
+    }
+    if (typeof data.body === "string") {
+      post.body = data.body;
+    }
+    if (typeof data.published === "boolean") {
+      post.published = data.published;
+    }
+    return post;
   },
   createComment(parent, { data }, { db }, info) {
     const postExist = db.posts.some(
@@ -93,6 +124,11 @@ const Mutation = {
     }
     const deletedComment = db.comments.splice(commentIndex, 1);
     return deletedComment[0];
+  },
+  updateComment(parent, { id, data }, { db }, info) {
+    const comment = db.comments.find(comment => comment.id === id);
+    if (typeof data.text === "string") comment.text = data.text;
+    return comment;
   }
 };
 
