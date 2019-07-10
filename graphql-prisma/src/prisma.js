bcc8b31c0d6e536f7874cf49b3e2cda91a2e076b
projectEscape <escaperealityproject@gmail.com>
2019-07-10 15:41:16 +0530

    Querying scalar data for users,posts from prisma

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -7,48 +7,50 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466"
 });
 
-const createPostForUser = async (authorId, data) => {
-  const userExists = await prisma.exists.User({ id: authorId });
+export default prisma;
 
-  if (!userExists) {
-    throw new Error("User not found");
-  }
+// const createPostForUser = async (authorId, data) => {
+//   const userExists = await prisma.exists.User({ id: authorId });
 
-  const post = await prisma.mutation.createPost(
-    {
-      data: {
-        ...data,
-        author: {
-          connect: {
-            id: authorId
-          }
-        }
-      }
-    },
-    "{ author { id name email posts{ id title published } } }"
-  );
-  return post.author;
-};
+//   if (!userExists) {
+//     throw new Error("User not found");
+//   }
 
-const updatePostForUser = async (postId, data) => {
-  const postExists = await prisma.exists.Post({ id: postId });
+//   const post = await prisma.mutation.createPost(
+//     {
+//       data: {
+//         ...data,
+//         author: {
+//           connect: {
+//             id: authorId
+//           }
+//         }
+//       }
+//     },
+//     "{ author { id name email posts{ id title published } } }"
+//   );
+//   return post.author;
+// };
 
-  if (!postExists) {
-    throw new Error("Post not found");
-  }
+// const updatePostForUser = async (postId, data) => {
+//   const postExists = await prisma.exists.Post({ id: postId });
 
-  const post = await prisma.mutation.updatePost(
-    {
-      where: {
-        id: postId
-      },
-      data
-    },
-    "{author{id name email posts{id title published}}}"
-  );
+//   if (!postExists) {
+//     throw new Error("Post not found");
+//   }
 
-  return post.author;
-};
+//   const post = await prisma.mutation.updatePost(
+//     {
+//       where: {
+//         id: postId
+//       },
+//       data
+//     },
+//     "{author{id name email posts{id title published}}}"
+//   );
+
+//   return post.author;
+// };
 
 // updatePostForUser("cjxqkqf4l00950880iss48oo6", {
 //   published: false
