88064aecfb48888db504a39f4c4960f0815afdb0
projectEscape <escaperealityproject@gmail.com>
2019-07-11 16:41:06 +0530

    Replacing query methods to use prisma

--- graphql-prisma/src/resolvers/Comment.js
+++ graphql-prisma/src/resolvers/Comment.js
@@ -1,13 +1,2 @@
-const Comment = {
-  author(parent, args, { db }, info) {
-    return db.users.find(user => {
-      return user.id === parent.author;
-    });
-  },
-  post(parent, args, { db }, info) {
-    return db.posts.find(post => {
-      return post.id === parent.post;
-    });
-  }
-};
+const Comment = {};
 export default Comment;
