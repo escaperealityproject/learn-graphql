50ccf2dedeafb294f10d813ce99082d0e5416bfe
projectEscape <escaperealityproject@gmail.com>
2019-07-16 18:24:05 +0530

    Using fragments to lock down email and unpublished posts

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,28 +1,17 @@
 import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
-import Query from "./resolvers/Query";
-import Mutation from "./resolvers/Mutation";
-import User from "./resolvers/User";
-import Post from "./resolvers/Post";
-import Comment from "./resolvers/Comment";
-import Subscription from "./resolvers/Subscription";
 import prisma from "./prisma";
+import { resolvers, fragmentReplacements } from "./resolvers/index";
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    User,
-    Post,
-    Comment,
-    Subscription
-  },
+  resolvers,
   context(request) {
     return { db, pubsub, prisma, request };
-  }
+  },
+  fragmentReplacements
 });
 
 server.start(() => {
--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,11 +1,11 @@
 import { Prisma } from "prisma-binding";
-import Query from "../../graphql-basics/src/resolvers/Query";
-import { FragmentsOnCompositeTypesRule } from "graphql";
+import { fragmentReplacements } from "./resolvers/index";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: "http://localhost:4466",
-  secret: "Icanputwhatevriwanthere"
+  secret: "Icanputwhatevriwanthere",
+  fragmentReplacements
 });
 
 export default prisma;
