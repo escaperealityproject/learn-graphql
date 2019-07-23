ae8a994091e7732892c23dac58d701b2af7f2929
projectEscape <escaperealityproject@gmail.com>
2019-07-23 16:07:01 +0530

    Basic jest test examples

--- graphql-prisma/test/user.test.js
+++ graphql-prisma/test/user.test.js
@@ -1,5 +1,31 @@
-test("First test case", () => {
-  1 + 2;
+import { getFirstName, isValidPassword } from "../src/utils/user.js";
+
+test("Should return first name when given full name", () => {
+  const firstName = getFirstName("Aniket Bansal");
+
+  expect(firstName).toBe("Aniket");
 });
 
-test("second test case", () => {});
+test("Should return first name when given first name", () => {
+  const firstName = getFirstName("Jen");
+
+  expect(firstName).toBe("Jen");
+});
+
+test("Should reject password shorter than 8 characters", () => {
+  const isValid = isValidPassword("abc123");
+
+  expect(isValid).toBe(false);
+});
+
+test("Should reject password that contains word password", () => {
+  const isValid = isValidPassword("abcPassword098");
+
+  expect(isValid).toBe(false);
+});
+
+test("Should correctly validate a valid password", () => {
+  const isValid = isValidPassword("Test123098!");
+
+  expect(isValid).toBe(true);
+});
