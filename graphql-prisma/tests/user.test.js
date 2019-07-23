5b7642525305562728c9d3c6d7e43315c556b4a9
projectEscape <escaperealityproject@gmail.com>
2019-07-24 00:10:55 +0530

    beforeEach lifecycle method in jest

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,9 +1,47 @@
 import "cross-fetch/polyfill";
 import ApolloBoost, { gql } from "apollo-boost";
+import bcrypt from "bcryptjs";
 import prisma from "../src/prisma";
 
 const client = new ApolloBoost({
   uri: "http://localhost:4000"
+});
+
+beforeEach(async () => {
+  jest.setTimeout(10000);
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: "baniket",
+      email: "baniket@example.com",
+      password: bcrypt.hashSync("password")
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "My published post",
+      body: "",
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: "My draft post",
+      body: "",
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
 });
 
 test("Should create a new user", async () => {
