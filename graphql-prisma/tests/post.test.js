1f3e0c7c6c1034bef4d1cb20f05e198ce8940902
projectEscape <escaperealityproject@gmail.com>
2019-07-25 16:03:58 +0530

    Refacored tests to use graphQL variables

--- graphql-prisma/tests/post.test.js
+++ graphql-prisma/tests/post.test.js
@@ -3,21 +3,19 @@ import { gql } from "apollo-boost";
 import seedDatabase, { userOne, postOne, postTwo } from "./utils/seedDatabase";
 import getClient from "./utils/getClient";
 import prisma from "../src/prisma";
+import {
+  getPosts,
+  myPosts,
+  updatePost,
+  createPost,
+  deletePost
+} from "./utils/operations";
 
 const client = getClient();
 
 beforeEach(seedDatabase);
+
 test("Should expose published posts", async () => {
-  const getPosts = gql`
-    query {
-      posts {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
   const response = await client.query({ query: getPosts });
 
   expect(response.data.posts.length).toBe(1);
@@ -26,16 +24,6 @@ test("Should expose published posts", async () => {
 
 test("Should fetch users posts", async () => {
   const client = getClient(userOne.jwt);
-  const myPosts = gql`
-    query {
-      myPosts {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
   const { data } = await client.query({ query: myPosts });
 
   expect(data.myPosts.length).toBe(2);
@@ -43,22 +31,13 @@ test("Should fetch users posts", async () => {
 
 test("Should be able to update own post", async () => {
   const client = getClient(userOne.jwt);
-  const updatePost = gql`
-        mutation {
-            updatePost(
-                id: "${postOne.post.id}",
-                data: {
-                    published: false
-                }
-            ){
-                id
-                title
-                body
-                published
-            }
-        }
-    `;
-  const { data } = await client.mutate({ mutation: updatePost });
+  const variables = {
+    id: postOne.post.id,
+    data: {
+      published: false
+    }
+  };
+  const { data } = await client.mutate({ mutation: updatePost, variables });
   const exists = await prisma.exists.Post({
     id: postOne.post.id,
     published: false
@@ -70,17 +49,10 @@ test("Should be able to update own post", async () => {
 
 test("Should create a new post", async () => {
   const client = getClient(userOne.jwt);
-  const createPost = gql`
-    mutation {
-      createPost(data: { title: "A test post", body: "", published: true }) {
-        id
-        title
-        body
-        published
-      }
-    }
-  `;
-  const { data } = await client.mutate({ mutation: createPost });
+  const variables = {
+    data: { title: "A test post", body: "", published: true }
+  };
+  const { data } = await client.mutate({ mutation: createPost, variables });
 
   expect(data.createPost.title).toBe("A test post");
   expect(data.createPost.body).toBe("");
@@ -89,16 +61,10 @@ test("Should create a new post", async () => {
 
 test("Should delete post", async () => {
   const client = getClient(userOne.jwt);
-  const deletePost = gql`
-        mutation {
-            deletePost(
-                id: "${postTwo.post.id}"
-            ) {
-                id
-            }
-        }
-    `;
-  await client.mutate({ mutation: deletePost });
+  const variables = {
+    id: postTwo.post.id
+  };
+  await client.mutate({ mutation: deletePost, variables });
   const exists = await prisma.exists.Post({ id: postTwo.post.id });
 
   expect(exists).toBe(false);
--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -3,31 +3,23 @@ import { gql } from "apollo-boost";
 import prisma from "../src/prisma";
 import seedDatabase, { userOne } from "./utils/seedDatabase";
 import getClient from "./utils/getClient";
+import { createUser, getUsers, login, getProfile } from "./utils/operations";
 
 const client = getClient();
 
 beforeEach(seedDatabase);
 
 test("Should create a new user", async () => {
-  const createUser = gql`
-    mutation {
-      createUser(
-        data: {
-          name: "Aniket"
-          email: "aniket@example.com"
-          password: "red12345"
-        }
-      ) {
-        token
-        user {
-          id
-        }
-      }
+  const variables = {
+    data: {
+      name: "Aniket",
+      email: "aniket@example.com",
+      password: "red12345"
     }
-  `;
-
+  };
   const response = await client.mutate({
-    mutation: createUser
+    mutation: createUser,
+    variables
   });
 
   const exists = await prisma.exists.User({
@@ -37,15 +29,6 @@ test("Should create a new user", async () => {
 });
 
 test("Should expose public author profiles", async () => {
-  const getUsers = gql`
-    query {
-      users {
-        id
-        name
-        email
-      }
-    }
-  `;
   const response = await client.query({ query: getUsers });
 
   expect(response.data.users.length).toBe(1);
@@ -54,44 +37,30 @@ test("Should expose public author profiles", async () => {
 });
 
 test("Should not login with bad credentials", async () => {
-  const login = gql`
-    mutation {
-      login(email: "baniket@example.com", password: "password123") {
-        token
-      }
+  const variables = {
+    data: {
+      email: "baniket@example.com",
+      password: "password123"
     }
-  `;
+  };
 
-  await expect(client.mutate({ mutation: login })).rejects.toThrow();
+  await expect(client.mutate({ mutation: login, variables })).rejects.toThrow();
 });
 
 test("Should not signup user with invalid password", async () => {
-  const createUser = gql`
-    mutation {
-      createUser(
-        data: { name: "Aniket", email: "aniket@example.com", password: "pass" }
-      ) {
-        token
-      }
-    }
-  `;
+  const variables = {
+    data: { name: "Aniket", email: "aniket@example.com", password: "pass" }
+  };
 
-  await expect(client.mutate({ mutation: createUser })).rejects.toThrow();
+  await expect(
+    client.mutate({ mutation: createUser, variables })
+  ).rejects.toThrow();
 });
 
 test("Should fetch user profile", async () => {
   const client = getClient(userOne.jwt);
-  const getProfile = gql`
-    query {
-      me {
-        id
-        name
-        email
-      }
-    }
-  `;
-
   const { data } = await client.query({ query: getProfile });
+
   expect(data.me.id).toBe(userOne.user.id);
   expect(data.me.name).toBe(userOne.user.name);
   expect(data.me.email).toBe(userOne.user.email);
