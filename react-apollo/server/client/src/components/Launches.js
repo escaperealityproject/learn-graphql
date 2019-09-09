7b4c67e3e4364cdb4f991b4bb0e277c6d407b720
projectEscape <escaperealityproject@gmail.com>
2019-09-09 17:03:15 +0530

    Rendering fetched data

--- react-apollo/server/client/src/components/Launches.js
+++ react-apollo/server/client/src/components/Launches.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import LaunchItem from "./LaunchItem";
 
 const LAUNCHES_QUERY = gql`
   query LaunchQuery {
@@ -16,7 +17,7 @@ const LAUNCHES_QUERY = gql`
 class Launches extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1 className="display-5 my-4">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
@@ -26,11 +27,18 @@ class Launches extends Component {
             if (error) {
               console.log(error);
             }
-            console.log(data);
-            return <h4>Data is fetched</h4>;
+            return (
+              <React.Fragment>
+                {data.launches.map(launch => {
+                  return (
+                    <LaunchItem key={launch.flight_number} launch={launch} />
+                  );
+                })}
+              </React.Fragment>
+            );
           }}
         </Query>
-      </div>
+      </>
     );
   }
 }
--- react-apollo/server/src/index.js
+++ react-apollo/server/src/index.js
@@ -2,6 +2,10 @@ import { GraphQLServer, PubSub } from "graphql-yoga";
 import axios from "axios";
 import Query from "./resolvers/Query";
 
+// // Not needed, but if making an express server, to allow croos-origin
+// import cors from "cors";
+// app.use(cors());
+
 const server = new GraphQLServer({
   typeDefs: "./schema.graphql",
   resolvers: { Query },
